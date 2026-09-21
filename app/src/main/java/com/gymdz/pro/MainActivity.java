f07242d0550323d8b482ed1d1709b108975d5fcb
حفيظ زياد <zyadhfyz86@gmail.com>
2026-09-21 11:25:05 +0100

    Add members foundation and Android app updates

--- app/src/main/java/com/gymdz/pro/MainActivity.java
+++ app/src/main/java/com/gymdz/pro/MainActivity.java
@@ -2,23 +2,39 @@ package com.gymdz.pro;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView w = new WebView(this);
-        w.setWebViewClient(new WebViewClient());
-        WebSettings s = w.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(true);
-        s.setBuiltInZoomControls(false);
-        s.setDisplayZoomControls(false);
-        w.loadUrl("file:///android_asset/index.html");
-        setContentView(w);
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(0xFF090A0C);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
